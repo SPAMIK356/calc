f875bd3553dbaafbcdc1c0a620396423db196345
SPAMIK <maximbondarenko20@gmail.com>
2024-10-04 20:11:29 +0300

    First commit

--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,12 +1,32 @@
 
 #include <iostream>
-
+using std::cout;
+using std::cin;
+using std::endl;
 int main()
 {
-    double a, b;
-
+    double a, b, result;
+    wrong:
     char input;
 
-
+    cout << "Input the expression (<Number> <Operator> <Number>)" << '\n';
+    
+    cin >> a >> input >> b;
+    switch (input)
+    {
+    case '+':
+        break;
+    case '-':
+        break;
+    case '*':
+        break;
+    case '/':
+        break;
+    default:
+        goto wrong;
+        break;
+    }
+    cout << a << input << b << '=' << result;
+    
 }
 
